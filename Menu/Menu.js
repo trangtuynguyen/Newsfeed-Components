ad8767d702442b20ded3c5aabaf91b606f783a11
Trang Nguyen <ttn2127@columbia.edu>
2019-07-11 13:44:31 -0400

    menu fixed

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,18 +1,56 @@
+let menuItems = [
+  'Students',
+  'Faculty',
+  "What's New",
+  'Tech Trends',
+  'Music',
+  'Log Out'
+];
 
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
+/* 
+  Step 1: Write a function that will create a menu component as seen below:
+  <div class="menu">
+    <ul>
+      {each menu item as a list item}
+    </ul>
+  </div>
+  Pass the function an array as it's only argument.
+  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+  Add those items to the <ul>
+  Step 3: Using a DOM selector, select the menu button currently on the DOM.
+  
+  Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
+  Step 5: return the menu component.
+  Step 6: add the menu component to the DOM.
+  
+*/
+let menu = createMenu(menuItems);
+console.log(menu);
+let body = document.querySelector('body');
+body.appendChild(menu);
 
+
+function createMenu(itemArray){
+  //set up new elements
+  const menuDiv = document.createElement('div');
+  const ul = document.createElement('ul');
+
+  //add class list
+  menuDiv.classList.add('menu');
+  //append item
+  itemArray.forEach(element => {
+    const li = document.createElement('li');
+    li.textContent = element;
+    ul.appendChild(li);
+  });
+
+  menuDiv.appendChild(ul);
+
+  let button = document.querySelector('.menu-button');
+  button.addEventListener('click', event=>{
+    menuDiv.classList.toggle('menu--open');
+  })
+
+  return menuDiv;
 }
-
-// Start Here: Create a reference to the ".menu" class
-
-let menu = document.querySelector('.menu');
-
-// create a reference to the ".menu-button" class
-
-let menuButton = document.querySelector('.menu-button');
-
-// Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', ()=>{toggleMenu()});
 
